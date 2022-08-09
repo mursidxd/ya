22be4b0e26f8bccc6357939681210a8ea820e17f
AhmdLui <104669426+LuiXyz@users.noreply.github.com>
2022-08-09 14:02:32 +0700

    Update yta.js

--- Yangtawutawuaja🤓/yta.js
+++ Yangtawutawuaja🤓/yta.js
@@ -1,48 +1,16 @@
-let limit = 30
 let fetch = require('node-fetch')
-const { servers, yta } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-┏┉━━━━━━━━━━━❏
-┆ *YOUTUBE MP3*
-├┈┈┈┈┈┈┈┈┈┈┈
-┆• *Judul:* ${title}
-│• *Type:* MP3
-┆• *📥 Ukuran File:* ${filesizeF}
-└❏
-`.trim(), m, null, {
-    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: true, contextInfo: {
-        externalAdReply: {
-            title: '▶︎ ━━━━━━━•────────────────── ', 
-            body: 'Now Playing...',
-            description: 'Now Playing...',
-            mediaType: 2,
-          thumbnail: await (await fetch(thumb)).buffer(),
-         mediaUrl: `https://youtube.com/watch?v=uIedYGN3NQQ`
-        }
-     }
-  })
-}
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
+let handler = async (m, { conn, args, text}) => {
+  if (!text) throw `Masukan Link nya!`
+  let json = await require("yuzzu-api").ytdl(text)
+
+await m.reply(wait)
+await conn.sendMessage(    m.chat,     { audio: { url: json.mp3 }, mimetype: 'audio/mp4' }, )}
+
+handler.help = ['yta <url>', 'ytmp3 <url>']
 handler.tags = ['downloader']
-handler.command = /^yt(a|mp3)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = /^(yta|ytmp3)$/i
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.register = true
 handler.limit = true
 
 module.exports = handler
